cbeb4bfa834badd34d57418040790a78e293a5cc
Zehra <zehratemizel727@gmail.com>
2023-12-25 22:59:43 +0300

    first commit

---
+++ Test_Case_Homework.py
@@ -0,0 +1,71 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+from selenium.webdriver.support.wait import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as ec 
+import pytest
+
+
+class Test_Case_Homework1: 
+    def setup_method(self): 
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://www.saucedemo.com")
+        self.driver.maximize_window() 
+
+    def teardown_method(self): 
+        self.driver.quit()
+
+
+#Kullanıcı adı ve şifre alanları boş geçildiğinde uyarı mesajı olarak "Epic sadface: Username is required" gösterilmelidir.
+    @pytest.mark.parametrize("username,password",[("","")]) 
+    def test_empty_login(self,username,password):
+        usernameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"user-name")))
+        usernameInput.send_keys(username)
+        passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"password")))
+        passwordInput.send_keys(password)
+        loginButton = self.driver.find_element(By.ID,"login-button")
+        loginButton.click()
+        errorMessage = self.driver.find_element(By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")
+        assert errorMessage.text == "Epic sadface: Username is required"
+
+#Sadece şifre alanı boş geçildiğinde uyarı mesajı olarak "Epic sadface: Password is required" gösterilmelidir.
+    @pytest.mark.parametrize("username,password",[("standard_user","")])     
+    def test_empty_password(self,username,password):
+        usernameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"user-name")))
+        usernameInput.send_keys(username)
+        passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"password")))
+        passwordInput.send_keys(password)
+        loginButton = self.driver.find_element(By.ID,"login-button")
+        loginButton.click()
+        errorMessage = self.driver.find_element(By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")
+        assert errorMessage.text == "Epic sadface: Password is required"
+
+
+#Kullanıcı adı "locked_out_user" şifre alanı "secret_sauce" gönderildiğinde "Epic sadface: Sorry, this user has been locked out." mesajı gösterilmelidir        
+    @pytest.mark.parametrize("username,password",[("locked_out_user","secret_sauce")])
+    def test_invalid_login(self,username,password):
+        usernameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"user-name")))
+        usernameInput.send_keys(username)
+        passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"password")))
+        passwordInput.send_keys(password)
+        loginButton = self.driver.find_element(By.ID,"login-button")
+        loginButton.click()
+        errorMessage = self.driver.find_element(By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")
+        assert errorMessage.text == "Epic sadface: Sorry, this user has been locked out."
+
+
+#Kullanıcı adı "standard_user" şifre "secret_sauce" gönderildiğinde kullanıcı "/inventory.html" sayfasına gönderilmelidir. Giriş yapıldıktan sonra kullanıcıya gösterilen ürün sayısı "6" adet olmalıdır.
+    @pytest.mark.parametrize("username,password",[("standard_user","secret_sauce")])       
+    def test_valid_login(self,username,password):
+        usernameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"user-name")))
+        usernameInput.send_keys(username)
+        passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"password")))
+        passwordInput.send_keys(password)
+        loginButton = self.driver.find_element(By.ID,"login-button")
+        loginButton.click()
+        listOfProducts = self.driver.find_elements(By.CLASS_NAME,"inventory_item")
+        assert len(listOfProducts)  == 6
+
+        
+
